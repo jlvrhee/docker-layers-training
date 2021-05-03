964fb75c09672e034f43cbd47eaa7a247b1fe782
Jeroen van Rhee <31248798+jlvrhee@users.noreply.github.com>
2021-05-03 13:22:25 +0200

    Update installdive.sh

--- installdive.sh
+++ installdive.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories
+echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk update
 apk add dive

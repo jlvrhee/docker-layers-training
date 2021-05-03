e10be7b7ede351ea787bcfcf64fb9e4d3643f57d
Jeroen van Rhee <31248798+jlvrhee@users.noreply.github.com>
2021-05-03 13:19:03 +0200

    Update installdive.sh

--- installdive.sh
+++ installdive.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "https://artifacts.kpn.org/alpine/edge/main" > /etc/apk/repositories
 echo "https://artifacts.kpn.org/alpine/edge/testing" >> /etc/apk/repositories
+apk update
+apk add dive
